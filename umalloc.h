75b374515a6ba81bc8a82d7b65698b190e5223d1
Nicholas Hoang <nicholash489@gmail.com>
2022-02-25 04:35:08 -0600

    added comments

--- umalloc.h
+++ umalloc.h
@@ -22,18 +22,32 @@ typedef struct memory_block_struct {
 *  STUDENT TODO:
 *      Write 1-2 sentences for each function explaining what it does. Don't just repeat the name of the function back to us.
 */
+// Does AND bitwise operator on block_size_alloc and 0x1 to determine if 
+// the block is marked as allocated.
 bool is_allocated(memory_block_t *block);
+// Changes bit 0 to 1 to denote the block as allocated.
 void allocate(memory_block_t *block);
+// Changes bit 0 to 0 to denote that the block is not allocated anymore.
 void deallocate(memory_block_t *block);
+// After ignoring the first 4 bits, return size of the block
 size_t get_size(memory_block_t *block);
+// Based on the block given, it will return the next member of the block.
 memory_block_t *get_next(memory_block_t *block);
+// Will initialize a given block with given values. block_size_alloc will be occupied
+// with the size and whether it is allocated or not. next will be set to NULL.
 void put_block(memory_block_t *block, size_t size, bool alloc);
+// adds 1 to the pointer of the given block and returns the payload.
 void *get_payload(memory_block_t *block);
+// subtracts 1 from the given pointer to return the memory block.
 memory_block_t *get_block(void *payload);
 
+// using the given size, find a block that is big enough to satisfy the needed size.
 memory_block_t *find(size_t size);
+// if needed, extend the heap to fit size of new block.
 memory_block_t *extend(size_t size);
+// split the block of memory into 2 new blocks: an allocated portion, and an unallocated portion.
 memory_block_t *split(memory_block_t *block, size_t size);
+// combines a free memory block with its neighbor.
 memory_block_t *coalesce(memory_block_t *block);
 
 
